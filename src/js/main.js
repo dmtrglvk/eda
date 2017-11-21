edddffccf4c678c27a49bebfbb93daef89108e8e
Dmytro Golovko <dmtrglvk@gmail.com>
2017-11-21 22:09:03 +0200

    index page responsive and adaptions; done

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,39 @@ $(document).ready(function () {
 	$('.js-select-style').styler();
 	$('.number-plus-minus input[type=number]').styler();
 
+	mobileMenu();
+	amountDrop();
+	$('.js-show-more').on('click', function(e){
+		e.preventDefault();
+		$(this).parent().addClass('alltext');
+		$(this).remove();
+	})
 
+});
 
-})
+function mobileMenu() {
+	$('.js-menu-opener').on('click', function(e){
+		e.preventDefault();
+		$('body').toggleClass('menu-opened');
+	})
+}
+
+function amountDrop() {
+	var amountOpener = $('.js-amount-opener'),
+		amountAdjuster = $('.js-amount-adjuster'),
+		amountValue = $('.js-amount-value'),
+		newAmount;
+
+	amountOpener.on('click', function(){
+		$(this).parent().find(amountAdjuster).addClass('opened-amountAdjuster').show();
+	});
+	$(document).on('mouseup', function(e){
+		if ($(e.target) != amountOpener && !$('.amount-box').find(e.target).length) {
+			var openedItem = $('.opened-amountAdjuster').closest('.amount-box');
+			newAmount = $('.opened-amountAdjuster').find('input[type=number]').val();
+			openedItem.find(amountValue).text(newAmount);
+			amountAdjuster.removeClass('opened-amountAdjuster').hide();
+		}
+	})
+}
+
