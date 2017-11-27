ed1878f4752a2aa23f05dbab31d55ed97892f36a
Dmytro Golovko <dmtrglvk@gmail.com>
2017-11-27 16:26:12 +0200

    cafe-review page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-//rating
-!function(t){t.fn.rating=function(i){i=i||function(){},this.each(function(n,a){t(a).data("rating",{callback:i}).bind("init.rating",t.fn.rating.init).bind("set.rating",t.fn.rating.set).bind("hover.rating",t.fn.rating.hover).trigger("init.rating")})},t.extend(t.fn.rating,{init:function(i){for(var n=t(this),a="",e=null,r=n.children(),s=0,l=r.length;l>s;s++)a=a+'<a class="star" title="'+t(r[s]).val()+'" />',t(r[s]).is(":checked")&&(e=t(r[s]).val());r.hide(),n.append('<div class="stars">'+a+"</div>").trigger("set.rating",e),t("a",n).bind("click",t.fn.rating.click),n.trigger("hover.rating")},set:function(i,n){var a=t(this),e=t("a",a),r=void 0;n&&(e.removeClass("fullStar"),r=e.filter(function(i){return t(this).attr("title")==n?t(this):!1}),r.addClass("fullStar").prevAll().addClass("fullStar"))},hover:function(i){var n=t(this),a=t("a",n);a.bind("mouseenter",function(i){t(this).addClass("tmp_fs").prevAll().addClass("tmp_fs"),t(this).nextAll().addClass("tmp_es")}),a.bind("mouseleave",function(i){t(this).removeClass("tmp_fs").prevAll().removeClass("tmp_fs"),t(this).nextAll().removeClass("tmp_es")})},click:function(i){i.preventDefault();var n=t(i.target),a=n.parent().parent(),e=a.children("input"),r=n.attr("title");matchInput=e.filter(function(i){return t(this).val()==r?!0:!1}),matchInput.attr("checked",!0).siblings("input").attr("checked",!1),a.trigger("set.rating",matchInput.val()).data("rating").callback(r,i)}})}(jQuery);
-
 $(document).ready(function () {
 
 	$('.js-slider').owlCarousel({
@@ -20,6 +17,7 @@ $(document).ready(function () {
 	slideBlock('Не указывать контактное лицо', 'Указывать контактное лицо');
 	showPassword();
 	orderDateDetails();
+	ratingBar();
 
 	$('.js-show-more').on('click', function(e){
 		e.preventDefault();
@@ -154,4 +152,13 @@ function orderDateDetails() {
 			$(this).closest('tr').next(slider).toggle();
 		})
 	}
+}
+
+function ratingBar() {
+	var item = $('.js-rating-bar li');
+
+	item.on('click', function () {
+		item.removeClass('active');
+		$(this).prevAll().addBack().addClass('active');
+	})
 }
