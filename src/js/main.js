7516bcfcc51d7d7c6ff982849b12816cc5c59724
Dmytro Golovko <dmtrglvk@gmail.com>
2017-12-04 20:51:02 +0200

    range slider fix

--- src/js/main.js
+++ src/js/main.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
 	});
 
 	if($( ".slider-range-bar").length){
+
 		$('.slider-range-bar').each(function(){
 			var $this = $(this);
 			var min = $this.data('min');
@@ -52,8 +53,24 @@ $(document).ready(function () {
 					$this.parent().find('input.min').trigger('change');
 				}
 			});
+			$('input.min').on('keydown', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||(/65|67|86|88/.test(e.keyCode)&&(e.ctrlKey===true||e.metaKey===true))&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+			$('input.max').on('keydown', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||(/65|67|86|88/.test(e.keyCode)&&(e.ctrlKey===true||e.metaKey===true))&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+
+			$('input.min').keyup(function(){
+				var maxVal = $('input.max').val();
+				$this.slider({
+					values: [ $(this).val(), maxVal ]
+				})
+			});
+			$('input.max').keyup(function(){
+				var minVal = $('input.min').val();
+				$this.slider({
+					values: [ minVal, $(this).val() ]
+				})
+			})
 		});
 	}
+
 	if($('input.mask').length) {
 		$('input.mask').inputmask({
 			mask: '+7 999 999-99-99'
